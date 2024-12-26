04a47ec3f34de48c635554c9024ea946c35f746d
Jason971013Huang <313037@s.lhvs.tyc.edu.tw>
2024-12-26 13:35:46 +0800

    1226 .C

--- 1226 .C
+++ 1226 .C
@@ -12,3 +12,19 @@ class mytime{
 		int getMinute(){return minute;}
 		void printTime(); 
 };
+bool myTime::validTime(int h,int m){
+	if(h<0||h>23)return false;
+	if(h<0||h>59)return false;	
+	return true;
+}
+bool myTime::setTime(int h,int m){
+	if(validTime(h,m)){
+		hour=h;
+		minute=m;
+		return true;
+	}
+	else return false;
+}
+void myTime::printTime(){
+	cout<<hour<<":"<<minute<<"\n";
+}
